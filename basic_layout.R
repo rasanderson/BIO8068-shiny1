e1d457a673ace9f92fe8bcebeda29f8a1880da36
Roy Sanderson <roy.sanderson@ncl.ac.uk>
2019-02-15 09:44:25 +0000

    Submit ad radio buttons added to sidebar

--- basic_layout.R
+++ basic_layout.R
@@ -6,7 +6,15 @@ wfarm <- base64enc::dataURI(file="www/UKwindfarm.png", mime="image/png")
 # Define UI ----
 ui <- fluidPage(
    sidebarLayout(
-      sidebarPanel("MySidebar"),
+      sidebarPanel("MySidebar",
+                   h3("a button"),
+                   actionButton(inputId="my_submitstatus", label="Submit"),
+      radioButtons(inputId = "my_checkGroup", 
+                         h3("Select one habitat"), 
+                         choices = list("Woodland" = 1, 
+                                        "Grassland" = 2, 
+                                        "Urban" = 3),
+                         selected = 1)),
       mainPanel(
          p("This website will be to help planners assess potential windfarm development
            areas in Cumbria, and achieve a ", strong("balance "), "between different",
